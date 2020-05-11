d99bb49304454031d97fc34d5b997a57535a31e0
Snehal Ranjan <snehalranjan5472@gmail.com>
2020-05-11 20:27:53 +0530

    Fixed Another Typo

--- Tests/DatasetsTests/OxfordIIITPets/OxfordIIITPets.swift
+++ Tests/DatasetsTests/OxfordIIITPets/OxfordIIITPets.swift
@@ -6,14 +6,14 @@ final class OxfordIIITPetsTests: XCTestCase {
     func testCreateOxfordIIITPets() {
         let dataset = OxfordIIITPets(batchSize: 64)
 
-        let batchCount = 0
+        var batchCount = 0
         for batch in dataset.training.sequenced() {
             batchCount += 1
             /// 3680 samples make 57 batches of size 64 and one batch of size 32
             let expectedBS = batchCount <= 57 ? 64 : 32
 
-            XCTAssert(batch.first.shape, [expectedBS, 224, 224, 3])
-            XCTAssert(batch.second.shape, [expectedBS, 224, 224, 1])
+            XCTAssertEqual(batch.first.shape, [expectedBS, 224, 224, 3])
+            XCTAssertEqual(batch.second.shape, [expectedBS, 224, 224, 1])
         }
         XCTAssertEqual(batchCount, dataset.training.count)
     }
